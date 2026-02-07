81164a9de07dae5955fb3e3d90f9c4fb15fa3c47
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-07 17:22:45 +0530

    use of HTTPException Class status code during the patient not found

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,HTTPException
 import json
 app = FastAPI()
 
@@ -21,11 +21,13 @@ def view():
     return data
 
 @app.get('/patient/{patient_id}')
-def view_patient(patient_id : str = Path(..., description ="Id of the patient in Db, example - 'P001' ")):
+def view_patient(patient_id : str = Path(..., description ="Id of the patient in Db" , Example = 'P001' )):
 
     #load all the patients
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
-    return {'error': "Patient data not found"}
+    raise HTTPException(status_code=404, detail='Patient Not found')
+
+    
