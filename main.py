3dbb655ce9d0a7f949499a678a3f4ff9c9c0b232
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-07 12:56:50 +0530

    use of path parameters to see the specific content from the json file /// And use of Path function to add the Descritpiton in the 'docs' file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 app = FastAPI()
 
@@ -18,5 +18,14 @@ def sarang():
 @app.get('/view')
 def view():
     data = load_data()
-
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id : str = Path(..., description ="Id of the patient in Db, example - 'P001' ")):
+
+    #load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': "Patient data not found"}
