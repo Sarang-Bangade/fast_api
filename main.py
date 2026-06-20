83a2a72de413a508f5fa25e6b201a2f6edf3118b
Sarang-Bangade <bangadesarang@gmail.com>
2026-06-20 23:58:53 +0530

    changes made

--- main.py
+++ main.py
@@ -80,10 +80,6 @@ order: str = Query('asc', description = 'sort in asc or desc order')):
     return sorted_data
 
 
-
-
-
-
 @app.post('/create')
 def create_patient(patient: Patient):
     #load existing data
