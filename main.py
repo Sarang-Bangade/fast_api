0b965a38218d8237001ad7aaf65f3edf0a0e24df
Sarang-Bangade <bangadesarang@gmail.com>
2026-06-22 23:19:19 +0530

    fast commit

--- main.py
+++ main.py
@@ -47,9 +47,10 @@ def save_data(data):
 def hello():
     return{'message':'Hello sarang here from fastApi'}
 
+
 @app.get('/about')
 def about():
-    return{'message' : 'Functional api to manage pateient records'}
+    return{'message':'Functional api to manage pateient records'}
 
 @app.get('/view')
 def view():
@@ -75,6 +76,7 @@ order: str = Query('asc', description = 'sort in asc or desc order')):
     if sort_by not in valid_fields:
         raise HTTPException (status_code= 400, detail='Invalid order select between asc and desc')
    
+
     data = load_data()
     sort_order = True if order ==' desc' else False
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse = sort_order)
